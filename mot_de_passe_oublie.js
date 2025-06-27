05280caa29f1081d9841577349a6ddf2f92cc5bf
Byerun-codes <byronafesse@gmail.com>
2025-06-27 12:45:10 +0100

    Update mot_de_passe_oublie.js

--- mot_de_passe_oublie.js
+++ mot_de_passe_oublie.js
@@ -1,9 +1,9 @@
 document.getElementById("resetForm").addEventListener("submit", function(e) {
   const email = document.getElementById("email").value;
   if (!email) {
-    alert("Veuillez entrer votre adresse email.");
+    alert("please enter your email.");
     e.preventDefault();
   } else {
-    alert("Un lien de réinitialisation sera envoyé si l'adresse existe dans notre base de données.");
+    alert("A reset link will be sent.");
   }
 });
