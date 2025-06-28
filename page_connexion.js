9b3740a42b53a903f2ad60e0558e7262c41427ba
helios2234 <habibtefack@gmail.com>
2025-06-28 12:11:34 +0100

    Update page_connexion.js

--- page_connexion.js
+++ page_connexion.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
   const inputs = document.querySelectorAll("input, select");
-
   inputs.forEach(input => {
     input.addEventListener("focus", () => {
       input.style.backgroundColor = "#e6f0ff";
@@ -9,12 +8,27 @@ document.addEventListener("DOMContentLoaded", () => {
       input.style.backgroundColor = "";
     });
   });
-//since page is in french, leave code as connexion
+
   const form = document.getElementById("loginForm");
   form.addEventListener("submit", e => {
+    e.preventDefault(); // Pour empêcher la soumission du formulaire
     const button = form.querySelector("button");
     button.textContent = "Connexion...";
     button.disabled = true;
     button.style.opacity = "0.6";
+
+    const userType = document.getElementById("user_type").value;
+    if (userType == "etudiant") {
+      window.location.href = "page_etudiants.html";
+    } else if (userType == "entreprise") {
+      window.location.href = "page_entreprises.html";
+    } else if (userType == "encadreur") {
+      window.location.href = "page_encadreurs.html";
+    } else {
+      alert("Veuillez sélectionner un type d'utilisateur");
+      button.textContent = "Connexion";
+      button.disabled = false;
+      button.style.opacity = "1";
+    }
   });
 });
